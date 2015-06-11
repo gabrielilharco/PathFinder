66ec29a69bed0f7a2e73ad2292ce7c392a7851db
Victor Pascoal <victorvpascoal@gmail.com>
2015-06-10 22:05:55 -0300

    Adding Configuration Manager

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,12 @@
 package Main;
 
+import Utils.ConfigManager;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println(ConfigManager.getInstance().getMapName());
 	}
 
 }
