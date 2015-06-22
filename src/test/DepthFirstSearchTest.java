e3076899e41f043f69e2a96067c121a491563f4b
alemuzio@aluno.ita.br <alemuzio@aluno.ita.br>
2015-06-22 20:00:44 -0300

    Adding first test files for Graph

---
+++ src/test/DepthFirstSearchTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author muzio
+ *
+ */
+public class DepthFirstSearchTest {
+
+	/**
+	 * Test method for {@link algorithm.DepthFirstSearch#run(representations.Graph, representations.Vertex, representations.Vertex)}.
+	 */
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+}
