26b114df058d7520479551dd8b173c7b09b59379
gabrielilharco <gabriel.ilharco@gmail.com>
2015-06-23 23:20:42 -0300

    sketching GUI

---
+++ src/graphicInterface/ImagePanel.java
@@ -0,0 +1,18 @@
+package graphicInterface;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+public class ImagePanel extends JPanel{
+	
+    private Image image;
+    public ImagePanel(Image image) {
+        this.image = image;
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.drawImage(image, 0, 0, null);
+    }
+}
